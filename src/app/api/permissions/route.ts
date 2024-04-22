6c9408eeeaf0acf6e125d2b181bab7f13959127c
Mohd Kaif <kaifmohd2014@gmail.com>
2024-04-22 23:06:08 +0530

    Refactor GET function in permissions/route.ts to use Request object instead of NextApiRequest

--- src/app/api/permissions/route.ts
+++ src/app/api/permissions/route.ts
@@ -1,9 +1,8 @@
 import axios from "axios";
-import { NextApiRequest, NextApiResponse } from "next";
 import { authOptions } from "@/auth";
 import { getServerSession } from "next-auth";
 
-export async function GET(req: NextApiRequest, res: NextApiResponse) {
+export async function GET(request: Request) {
   const session: any = await getServerSession(authOptions);
   try {
     // Make a GET request to fetch user permissions from the backend
