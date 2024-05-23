df785624329cee924b02025fae81cab4f4c2fb9c
Mohd Kaif <kaifmohd2014@gmail.com>
2024-05-23 17:23:19 +0530

    feat: Update product page to include dynamic document title and meta description

--- src/app/(home)/product/[slug]/page.tsx
+++ src/app/(home)/product/[slug]/page.tsx
@@ -2,10 +2,18 @@ import ProductShare from "@/components/component/product-share";
 import React from "react";
 
 const Page = ({ params: { slug } }: any) => {
+  const fetchProduct = async () => {
+    const response = await fetch(`/api/product/${slug}`);
+    const data = await response.json();
+    return data;
+  };
+
+  const data: any = fetchProduct();
+
   return (
     <>
       <head>
-        <title>Product Share</title>
+        <title>Product Share {data["findProduct"].name}</title>
         <meta name="description" content="Product Share" />
       </head>
       <ProductShare params={{ slug }} />
