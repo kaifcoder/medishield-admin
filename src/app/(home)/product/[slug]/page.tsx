73bd567cd57266cf97f04ecdc58cdbcb8c1c6a1b
Mohd Kaif <kaifmohd2014@gmail.com>
2024-05-23 17:45:34 +0530

    feat: Update product page title in metadata object

--- src/app/(home)/product/[slug]/page.tsx
+++ src/app/(home)/product/[slug]/page.tsx
@@ -3,7 +3,7 @@ import { Metadata } from "next";
 import React from "react";
 
 export const metadata: Metadata = {
-  title: "MediShield",
+  title: "Checkout this product on Medishield",
   description: "MediShield - A dental supplies store",
 };
 
