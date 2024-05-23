a153300593382c1e8852a6f508899deae5811329
Mohd Kaif <kaifmohd2014@gmail.com>
2024-05-23 17:21:13 +0530

    feat: Refactor product page to use ProductShare component

--- src/app/(home)/product/[slug]/page.tsx
+++ src/app/(home)/product/[slug]/page.tsx
@@ -1,49 +1,14 @@
-"use client";
-import { ProductDetails } from "@/components/component/product-details";
-import React, { useEffect, useState } from "react";
+import ProductShare from "@/components/component/product-share";
+import React from "react";
 
 const Page = ({ params: { slug } }: any) => {
-  const [product, setproduct] = useState({}) as any;
-  const [loading, setLoading] = useState(true);
-  console.log(slug);
-  const fetchProduct = async () => {
-    setLoading(true);
-    const response = await fetch(`/api/product/${slug}`);
-    const data = await response.json();
-    setproduct(data["findProduct"]);
-    setLoading(false);
-  };
-
-  useEffect(() => {
-    fetchProduct();
-  }, [slug]);
-
-  useEffect(() => {
-    document.title = product.name;
-    document
-      .querySelector('meta[name="description"]')
-      ?.setAttribute("content", product.short_description);
-  }, [product]);
-
   return (
     <>
       <head>
-        <title>{product.name || "Check out this Product"}</title>
-        <meta name="description" content={product.short_description || ""} />
+        <title>Product Share</title>
+        <meta name="description" content="Product Share" />
       </head>
-      <div className="p-8 h-full flex justify-center">
-        {!loading ? (
-          <ProductDetails key={product.sku} isView={true} product={product} />
-        ) : (
-          <div
-            className="
-        flex h-full items-center justify-center
-        "
-          >
-            <div className="animate-spin rounded-full h-32 w-32 border-t-2 border-b-2 border-gray-900"></div>
-          </div>
-        )}
-      </div>
+      <ProductShare params={{ slug }} />
     </>
   );
 };
