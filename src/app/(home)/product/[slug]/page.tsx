8c1cb2f975e8587f160acad77b8af9d7feede073
Mohd Kaif <kaifmohd2014@gmail.com>
2024-05-23 17:42:22 +0530

    Refactor product page to remove unnecessary console.log statements

--- src/app/(home)/product/[slug]/page.tsx
+++ src/app/(home)/product/[slug]/page.tsx
@@ -1,23 +1,15 @@
 import ProductShare from "@/components/component/product-share";
+import { Metadata } from "next";
 import React from "react";
 
+export const metadata: Metadata = {
+  title: "MediShield",
+  description: "MediShield - A dental supplies store",
+};
+
 const Page = ({ params: { slug } }: any) => {
-  const fetchProduct = async () => {
-    const response = await fetch(
-      `${process.env.API_URL}/api/product/getproductwithid/${slug}`
-    );
-    const data = await response.json();
-    console.log(data);
-    return data;
-  };
-
-  fetchProduct();
-
   return (
     <>
-      <head>
-        <title>Product Share </title>
-      </head>
       <ProductShare params={{ slug }} />
     </>
   );
